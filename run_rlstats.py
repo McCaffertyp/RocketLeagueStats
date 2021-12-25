b117b23fc2de3b1b57fe8b66ef839c961c74df70
Paul <fifaplus@comcast.net>
2021-12-25 09:04:00 -0800

    Added some stuff to querying files. Added back try-except code for selenium

--- run_rlstats.py
+++ run_rlstats.py
@@ -1,3 +1,18 @@
+import sys
+import subprocess
+
+try:
+    from selenium import webdriver
+except ImportError:
+    print("Failed to import webdriver from selenium")
+    print("Attempting manual install")
+    try:
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'selenium'])
+    except subprocess.CalledProcessError:
+        print("Error running install command 'pip install selenium'")
+finally:
+    from selenium import webdriver
+
 from mccaffertyp.rl_stats import RLStats
 from flask import Flask, render_template
 
